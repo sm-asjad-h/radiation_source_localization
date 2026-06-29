5177d487537012a1024a076dd48985bb5eb8e5a6
sm-asjad-h <syedasjad04@gmail.com>
2026-06-29 06:05:17 +0000

    updated functions for accurate indexings

--- common_function/ML_model_training.py
+++ common_function/ML_model_training.py
@@ -4,11 +4,12 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import SVR
 from xgboost import XGBRegressor
 import pandas as pd
+import numpy as np
 def train_loc_models(X_train, y_train, X_test):
     print("Training Location Models...")
     dt_model_loc = DecisionTreeRegressor(random_state=42)
     rf_model_loc = RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
-    xgb_model_loc = MultiOutputRegressor(XGBRegressor(n_estimators=100, random_state=42, n_jobs=-1))
+    xgb_model_loc = XGBRegressor(n_estimators=100, random_state=42, n_jobs=-1, tree_method='hist')
     svm_model_loc = MultiOutputRegressor(SVR(kernel='rbf'))
 
     dt_model_loc.fit(X_train, y_train)
@@ -17,10 +18,10 @@ def train_loc_models(X_train, y_train, X_test):
     svm_model_loc.fit(X_train, y_train)
 
     models_preds = {
-        "Decision Tree": pd.DataFrame(dt_model_loc.predict(X_test), columns=['source_x', 'source_y']),
-        "Random Forest": pd.DataFrame(rf_model_loc.predict(X_test), columns=['source_x', 'source_y']),
-        "XGBoost": pd.DataFrame(xgb_model_loc.predict(X_test), columns=['source_x', 'source_y']),
-        "SVM": pd.DataFrame(svm_model_loc.predict(X_test), columns=['source_x', 'source_y'])
+        "Decision Tree": pd.DataFrame(dt_model_loc.predict(X_test), columns=['source_x', 'source_y'], index=X_test.index),
+        "Random Forest": pd.DataFrame(rf_model_loc.predict(X_test), columns=['source_x', 'source_y'], index=X_test.index),
+        "XGBoost": pd.DataFrame(xgb_model_loc.predict(X_test), columns=['source_x', 'source_y'], index=X_test.index),
+        "SVM": pd.DataFrame(svm_model_loc.predict(X_test), columns=['source_x', 'source_y'], index=X_test.index)
     }
     return models_preds
 def train_int_models(X_train, y_train, X_test):
@@ -40,5 +41,26 @@ def train_int_models(X_train, y_train, X_test):
         "Random Forest Intensity": rf_model_intensity.predict(X_test),
         "XGBoost Intensity": xgb_model_intensity.predict(X_test),
         "SVM Intensity": svm_model_intensity.predict(X_test)
+    }
+    return models_preds
+def train_int_models_log(X_train, y_train, X_test):
+    print("Training Intensity Models...")
+    dt_model_intensity = DecisionTreeRegressor(random_state=42)
+    rf_model_intensity = RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
+    xgb_model_intensity = XGBRegressor(n_estimators=100, random_state=42, n_jobs=-1, tree_method='hist')
+    svm_model_intensity = SVR(kernel='rbf')
+
+    y_train_log = np.log(y_train)
+
+    dt_model_intensity.fit(X_train, y_train_log)
+    rf_model_intensity.fit(X_train, y_train_log)
+    xgb_model_intensity.fit(X_train, y_train_log)
+    svm_model_intensity.fit(X_train, y_train_log)
+
+    models_preds = {
+        "Decision Tree Intensity": pd.Series(np.exp(dt_model_intensity.predict(X_test)), index=X_test.index),
+        "Random Forest Intensity": pd.Series(np.exp(rf_model_intensity.predict(X_test)), index=X_test.index),
+        "XGBoost Intensity": pd.Series(np.exp(xgb_model_intensity.predict(X_test)), index=X_test.index),
+        "SVM Intensity": pd.Series(np.exp(svm_model_intensity.predict(X_test)), index=X_test.index)
     }
     return models_preds
--- common_function/split_and_scale.py
+++ common_function/split_and_scale.py
@@ -1,22 +1,22 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-def split_and_scale_data(df, scale=False):
+def split_and_scale_data(df, scale=True):
     df = df.copy()
-
     X = df.drop(columns=['source_x', 'source_y', 'I_0'])
-    y = df[['source_x', 'source_y','I_0']]
+    y = df[['source_x', 'source_y', 'I_0']]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    y_train_loc=y_train.drop(columns=['I_0'])
-    y_test_loc=y_test.drop(columns=['I_0'])
-    y_train_int=y_train['I_0']
-    y_test_int=y_test['I_0']
+    
+    y_train_loc = y_train[['source_x', 'source_y']]
+    y_test_loc = y_test[['source_x', 'source_y']]
+    y_train_int = y_train['I_0']
+    y_test_int = y_test['I_0']
 
     X_test_unscaled = X_test.copy()
 
     if scale:
         scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
+        X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X.columns, index=X_train.index)
+        X_test = pd.DataFrame(scaler.transform(X_test), columns=X.columns, index=X_test.index)
 
     return X_train, X_test, y_train_loc, y_test_loc, y_train_int, y_test_int, X_test_unscaled
